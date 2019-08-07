91ac3370dc18272e05977c5e60bb0b91b42ed9a9
Hao Wu <wuhaocll@gmail.com>
2019-08-07 14:32:24 +0200

    Initial Commit

---
+++ TestJSONParse/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestJSONParse
+//
+//  Created by Hao Wu on 07.08.19.
+//  Copyright © 2019 Hao Wu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
